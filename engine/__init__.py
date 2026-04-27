04f41451a3be0268b6df8b5bff3fa7177605fd3b
juho <juho@uusi-luomalahti@slamengine.com>
2026-04-27 09:29:47 +0000

    remove files for rewrite of lsp

--- engine/__init__.py
+++
@@ -1,5 +0,0 @@
-from .config import load_config
-from .core import AivoEngine
-from .types import EngineConfig, RepoConfig, LspLaunchConfig
-
-__all__ = ["AivoEngine", "EngineConfig", "RepoConfig", "LspLaunchConfig", "load_config"]
